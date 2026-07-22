8deabaf2ed0e05218c79516504fce9a1e1821d23
ram2005024 <sharmashekhar20050@gmail.com>
2026-07-22 16:10:37 +0545

    login with phone and email ui and form hook

--- frontend/src/components/auth/login/EmailLoginForm.tsx
+++ frontend/src/components/auth/login/EmailLoginForm.tsx
@@ -1,21 +1,41 @@
 import React from 'react'
 import { Input } from '../../ui/input'
 import { Button } from '../../ui/button'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { emailLoginSchema, emailLoginType } from '../../../schemas/auth/LoginSchema'
 
 const EmailLoginForm = () => {
+  const form = useForm<emailLoginType>({
+    defaultValues: {
+      email: '',
+      password: ''
+    },
+    resolver: zodResolver(emailLoginSchema)
+  })
+const handleEmailLogin=(data:emailLoginType)=>{
+  console.log(data)
+}
   return (
-   <form className="mt-4 space-y-4">
-     <Input placeholder="you@example.com" />
-              <Input type="password" placeholder="Password" />
-              <div className="flex justify-end">
-                <Button variant="link" className="text-xs text-gray-500">
-                  Forgot password?
-                </Button>
-              </div>
-              <Button className="w-full bg-orange-500 hover:bg-orange-600 text-white">
-                Sign in →
-              </Button>
-   </form>
+    <form className="mt-4 space-y-4" onSubmit={form.handleSubmit(handleEmailLogin)}>
+      <Input
+        placeholder="you@example.com"
+        {...form.register('email')}
+      />
+      <Input
+        type="password"
+        placeholder="Password"
+        {...form.register('password')}
+      />
+      <div className="flex justify-end">
+        <Button variant="link" className="text-xs text-gray-500">
+          Forgot password?
+        </Button>
+      </div>
+      <Button className="w-full bg-orange-500 hover:bg-orange-600 text-white">
+        Sign in →
+      </Button>
+    </form>
   )
 }
 
--- frontend/src/components/auth/login/PhoneLoginForm.tsx
+++ frontend/src/components/auth/login/PhoneLoginForm.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import { Input } from '../../ui/input'
 import { Button } from '../../ui/button'
+import { useForm } from 'react-hook-form'
+import { phoneLoginSchema, phoneLoginType } from '../../../schemas/auth/LoginSchema'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 const PhoneLoginForm = () => {
+  const form=useForm<phoneLoginType>({
+  defaultValues:{
+    mobile_number:"",
+    password:""
+  },
+  resolver:zodResolver(phoneLoginSchema)
+})
+const handleRegisterLogin=(data:phoneLoginType)=>{
+  console.log(data)
+}
   return (
-    <form className="mt-4 space-y-4">
-          <Input placeholder="+977 98••••••••" />
-              <Input type="password" placeholder="Password" />
+    <form className="mt-4 space-y-4" onSubmit={form.handleSubmit(handleRegisterLogin)}>
+          <Input placeholder="+977 98••••••••" {...form.register('mobile_number')}/>
+              <Input type="password" placeholder="Password" {...form.register('mobile_number')} />
               <div className="flex justify-end">
                 <Button variant="link" className="text-xs text-gray-500">
                   Forgot password?
