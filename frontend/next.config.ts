67d41b145efcb24979e4188b8e8b3523dfc4894a
ram2005024 <sharmashekhar20050@gmail.com>
2026-08-18 21:46:21 +0545

    init

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-images:{
-  domains:["res.cloudinary.com","lh3.googleusercontent.com"]
-}
+  images: {
+    domains: ["res.cloudinary.com", "lh3.googleusercontent.com"],
+  },
 };
 
 export default nextConfig;
--- frontend/src/components/auth/forgetPassword/forgetByEmail.tsx
+++ frontend/src/components/auth/forgetPassword/forgetByEmail.tsx
@@ -6,34 +6,42 @@ import { Mail } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { AxiosError } from "axios";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 import z from "zod";
 import { useForget } from "../../../hooks/auth/useForget";
-import { useState } from "react";
-import { AxiosError } from "axios";
-import toast from "react-hot-toast";
 import VerifyForget from "./verifyForget";
 
 export default function ForgotByEmail() {
-    const [open,setOpen]=useState<boolean>(false)
-    const forgetByEmailMutation=useForget()
-    const [userID,setUserID]=useState<string>("")
-    const forget_schema_email=z.object({
-        email:z.email("Invalid email")
-    })
-    const form=useForm<{email:string}>({
-        defaultValues:{
-            email:"",
-        },
-        resolver:zodResolver(forget_schema_email)
-    })
+  const [open, setOpen] = useState<boolean>(false);
+  const forgetByEmailMutation = useForget();
+  const [userID, setUserID] = useState<string>("");
+  const forget_schema_email = z.object({
+    email: z.email("Invalid email"),
+  });
+  const form = useForm<{ email: string }>({
+    defaultValues: {
+      email: "",
+    },
+    resolver: zodResolver(forget_schema_email),
+  });
   return (
     <div className="space-y-6 pt-5">
-        {/* Forget success model */}
-        {open &&
-        userID && <VerifyForget open={open} onClose={()=>setOpen(false)} data={{user_id:userID,field_name:"email",field_value:form.getValues("email")}}/>
-        }
+      {/* Forget success model */}
+      {open && userID && (
+        <VerifyForget
+          open={open}
+          onClose={() => setOpen(false)}
+          data={{
+            user_id: userID,
+            field_name: "email",
+            field_value: form.getValues("email"),
+          }}
+        />
+      )}
       {/* Email */}
       <div>
         <label className="mb-2 block text-sm font-medium text-gray-700">
@@ -62,30 +70,41 @@ export default function ForgotByEmail() {
           />
         </div>
 
-        <p className="mt-2 text-xs text-gray-500">
+        <p className="mt-2.5 text-xs text-gray-500">
           Enter the email associated with your account.
         </p>
         {form.formState.errors?.email && (
-            <p className="text-xs mt-2 text-red-600">{form.formState.errors?.email?.message}</p>
+          <p className="text-xs mt-2 text-red-600">
+            {form.formState.errors?.email?.message}
+          </p>
         )}
       </div>
 
       {/* Button */}
-      <Button onClick={()=>forgetByEmailMutation.mutate({email:form.getValues("email")},{
-        onSuccess:(data)=>{
-            setUserID(data.user_id)
-            setOpen(true)
-        },
-        onError:(err)=>{
-            const error = err as AxiosError<{ message?: string }>;
-            toast.error(
-              error.response?.data?.message ||
-                error.message ||
-                "Something went wrong"
-            );
+      <Button
+        onClick={() =>
+          forgetByEmailMutation.mutate(
+            { email: form.getValues("email") },
+            {
+              onSuccess: (data) => {
+                setUserID(data.user_id);
+                setOpen(true);
+              },
+              onError: (err) => {
+                const error = err as AxiosError<{ message?: string }>;
+                toast.error(
+                  error.response?.data?.message ||
+                    error.message ||
+                    "Something went wrong",
+                );
+              },
+            },
+          )
         }
-      })} disabled={forgetByEmailMutation.isPending} className="h-12 w-full rounded-xl bg-orange-500 font-semibold transition hover:bg-orange-600">
-       {forgetByEmailMutation.isPending ? "Sending...":"Send Reset OTP"}
+        disabled={forgetByEmailMutation.isPending}
+        className="h-12 w-full rounded-xl bg-orange-500 font-semibold transition hover:bg-orange-600"
+      >
+        {forgetByEmailMutation.isPending ? "Sending..." : "Send Reset OTP"}
       </Button>
 
       {/* Footer */}
