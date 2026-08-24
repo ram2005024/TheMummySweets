ee9634fe0b1fe8da78146545a0388e9efe0274f8
ram2005024 <sharmashekhar20050@gmail.com>
2026-08-24 07:28:22 +0545

    add_wishlish_service initialized

--- backend/app/modules/menu/services/wishlist_service.py
+++ backend/app/modules/menu/services/wishlist_service.py
@@ -1,6 +1,11 @@
+from uuid import UUID
+
 from app.modules.menu.repos.wishlist_repo import WishlistRepo
 
 
 class WishlistService:
     def __init__(self, wishlist_repo: WishlistRepo) -> None:
         self.wishlist_repo = wishlist_repo
+
+    async def add_product_to_wishlist(self, user_id: UUID, product_id: UUID):
+        pass
