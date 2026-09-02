7abe0f5fdb78fd817b29bd3831d5e6f02b45145d
ram2005024 <sharmashekhar20050@gmail.com>
2026-09-02 12:02:08 +0545

    exception handled for image using sentry

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,3 +1,4 @@
+from app.modules.order.models.order_model import OrderModel  # Noqa
 from app.modules.auth.models.user import User, Profile  # NOQA
 from app.modules.auth.models.auth_sessions import Session  # noqa
 from app.modules.menu.models.category_model import Category  # noqa
@@ -6,6 +7,5 @@ from app.modules.menu.models.product_model import Product  # noqa
 from app.modules.menu.models.review_model import Comment, Review  # noqa
 from app.modules.menu.models.wishlist_model import WishList  # Noqa
 from app.modules.order.models.order_item_model import OrderItem  # Noqa
-from app.modules.order.models.order_model import OrderModel  # Noqa
 from app.modules.order.models.payment_model import PaymentModel  # Noqa
 from app.modules.order.models.coupen_model import CoupenModel  # Noqa
--- backend/app/tasks/email_task.py
+++ backend/app/tasks/email_task.py
@@ -1,29 +1,26 @@
-
-
-
-
 from app.core.celery_app import celery_app
 from app.services.notification_service import NotificationService
 
 
 @celery_app.task(bind=True, max_retries=3, default_retry_delay=60)
-def send_otp(self, otp: str,source:str,value:list[str], name: str):
+def send_otp(self, otp: str, source: str, value: list[str], name: str):
     try:
-        if source=="email":
-            result=NotificationService.email_otp(otp, name, value)
+        if source == "email":
+            result = NotificationService.email_otp(otp, name, value)
         else:
-            result=NotificationService.send_sms_otp(value[0],otp,name)
+            result = NotificationService.send_sms_otp(value[0], otp, name)
         return str(result)
     except Exception as exc:
         raise self.retry(exc=exc)
 
+
 @celery_app.task(bind=True, max_retries=3, default_retry_delay=60)
-def send_reset_otp(self, otp: str,source:str,value:list[str], name: str):
+def send_reset_otp(self, otp: str, source: str, value: list[str], name: str):
     try:
-        if source=="email":
-            result=NotificationService.reset_email_otp(otp, name, value)
+        if source == "email":
+            result = NotificationService.reset_email_otp(otp, name, value)
         else:
-            result=NotificationService.send_reset_sms_otp(otp,name,value[0])
+            result = NotificationService.send_reset_sms_otp(otp, name, value[0])
         return str(result)
     except Exception as exc:
         raise self.retry(exc=exc)
