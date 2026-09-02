42cb19ccf04fc02f202bb42d98ac74ca7a3e10e5
ram2005024 <sharmashekhar20050@gmail.com>
2026-09-02 11:19:56 +0545

    deleted the image tabel

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -4,7 +4,6 @@ from app.modules.menu.models.category_model import Category  # noqa
 from app.modules.menu.models.relationship_model import category_product  # noqa
 from app.modules.menu.models.product_model import Product  # noqa
 from app.modules.menu.models.review_model import Comment, Review  # noqa
-from app.models.image import Image  # Noqa
 from app.modules.menu.models.wishlist_model import WishList  # Noqa
 from app.modules.order.models.order_item_model import OrderItem  # Noqa
 from app.modules.order.models.order_model import OrderModel  # Noqa
