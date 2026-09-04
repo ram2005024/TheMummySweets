dbe157a1d060ab165b980b8bf98fe9ed27db08b8
ram2005024 <sharmashekhar20050@gmail.com>
2026-09-04 22:43:05 +0545

    wishlist sticky bug fixed

--- frontend/src/components/Nav/WishlistbButton.tsx
+++ frontend/src/components/Nav/WishlistbButton.tsx
@@ -1,10 +1,12 @@
 import { useGetWishlistProducts } from "@/hooks/menu/useMenuItems";
 import { useWishlistStore } from "@/store/wishlist.store";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { LuHeart } from "react-icons/lu";
 
 const WishlistButton = () => {
   const { data, isPending } = useGetWishlistProducts();
+  const router = useRouter();
   const { setWishlistCount, wishlist_count } = useWishlistStore();
   useEffect(() => {
     const hasData = data?.pages[0].products.meta.total;
@@ -17,6 +19,7 @@ const WishlistButton = () => {
     <button
       type="button"
       aria-label="Wishlist"
+      onClick={() => router.push("/wishlist")}
       className="relative flex h-9 w-9 cursor-pointer items-center justify-center text-[#554941] transition-all hover:text-[#e95d48]"
     >
       <LuHeart size={20} strokeWidth={1.7} />
