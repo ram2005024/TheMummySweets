78e83836d7e95f9447295102ef75759ddb34cd80
ram2005024 <sharmashekhar20050@gmail.com>
2026-09-05 20:15:46 +0545

    order service and repo file initialized

--- backend/app/modules/order/service/order_service.py
+++ backend/app/modules/order/service/order_service.py
@@ -1,6 +1,10 @@
 from app.modules.order.repo.order_repo import OrderRepo
+from app.modules.order.schemas.order_schema import OrderRequest
 
 
 class OrderService:
     def __init__(self, order_repo: OrderRepo) -> None:
         self.order_repo = order_repo
+
+    async def create_order(self, data: OrderRequest):
+        pass
