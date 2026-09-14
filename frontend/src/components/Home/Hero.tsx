cfb7f72f4e01b0ecdedcf27b44c11334b8987208
ram2005024 <sharmashekhar20050@gmail.com>
2026-09-14 16:04:41 +0545

    updated ci cd

--- frontend/src/components/Home/Hero.tsx
+++ frontend/src/components/Home/Hero.tsx
@@ -40,7 +40,7 @@ const Hero = () => {
           &quot;Pure taste from the hands of Mom &quot;
         </h2>
         <span className="text-gray-800 font-light">
-          Butwal-15,Semlar,Mahadaiya Chowk
+          Butwal-15,Semlar,Mahadaiya chowk
         </span>
       </div>
     </div>
