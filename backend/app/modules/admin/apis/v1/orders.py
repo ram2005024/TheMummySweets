0d91ca924954993ac1442ce2e8d2db9344cb32cf
ram2005024 <sharmashekhar20050@gmail.com>
2026-09-17 18:04:46 +0545

    get orders admin api

--- backend/app/modules/admin/apis/v1/orders.py
+++ backend/app/modules/admin/apis/v1/orders.py
@@ -5,7 +5,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import get_db
 from app.dependencies.permission import RolePermission
+from app.modules.admin.dependencies.get_service_factories import get_order_service_admin
 from app.modules.admin.schemas.order_schema import ReadOrdersAdmin
+from app.modules.admin.services.order_service import OrderServiceAdmin
 from app.modules.auth.models.user import User
 from app.schemas.common import SuccessResponse
 from app.websocket.dependencies import verify_socket_connection
@@ -32,5 +34,7 @@ async def establish_connection(
 @admin_order_router.get("/", response_model=SuccessResponse[list[ReadOrdersAdmin]])
 async def get_admin_orders_endpoint(
     user: Annotated[User, Depends(RolePermission(["admin"]))],
+    order_admin_service: Annotated[OrderServiceAdmin, Depends(get_order_service_admin)],
 ):
-    pass
+    orders = await order_admin_service.get_order_admin()
+    return SuccessResponse(data=orders)
